e17f651cfe45e0ddad763984ceaeacda3fffbd62
Saleem Rashid <dev@saleemrashid.com>
2017-08-01 16:07:36 +0100

    util: Use different data directory

--- lib/util.py
+++ lib/util.py
@@ -242,11 +242,11 @@ def user_dir():
     if 'ANDROID_DATA' in os.environ:
         return android_check_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-cash")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-Cash")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-Cash")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
