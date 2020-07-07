840b7549b3bc74796714d5a0d0b0c4ceb87e9bba
Wenfeng Wang <kalot.wang@gmail.com>
2020-07-07 11:52:37 +0800

    Fix ElectrumG binary build issues

--- contrib/osx/package.sh
+++ contrib/osx/package.sh
@@ -73,16 +73,16 @@ ${genisoimage} \
     -D \
     -l \
     -probe \
-    -V "Electrum" \
+    -V "ElectrumG" \
     -no-pad \
     -r \
     -dir-mode 0755 \
     -apple \
-    -o Electrum_uncompressed.dmg \
+    -o ElectrumG_uncompressed.dmg \
     /tmp/electrum-macos/image || fail "Unable to create uncompressed dmg"
 
-dmg dmg Electrum_uncompressed.dmg electrum-$VERSION.dmg || fail "Unable to create compressed dmg"
-rm Electrum_uncompressed.dmg
+dmg dmg ElectrumG_uncompressed.dmg electrumg-$VERSION.dmg || fail "Unable to create compressed dmg"
+rm ElectrumG_uncompressed.dmg
 
 echo "Done."
-sha256sum electrum-$VERSION.dmg
+sha256sum electrumg-$VERSION.dmg
